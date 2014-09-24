b1313f681442f662b7dd56aae95edfb85cb6edfe
hatemfaheem <hatem.faheem@gmail.com>
2014-09-24 12:42:26 +0300

    ZigZag conversion latest solution O(1) memory and O(n) time

--- ZigZagConversion.cpp
+++ ZigZagConversion.cpp
@@ -7,19 +7,15 @@ public:
         string res="";
         for (int row=0; row<nRows; row++)
         {
-            int inc = 2 + 2*(nRows-2);
-            int dnInc = 2 + 2*(nRows-2-row);
-            int upInc = 2 + 2*(row-1);
-
             bool down=true;
             for (int i=row; i<s.size(); )
             {
                 res += s[i];
                 if (row == 0 || row == nRows-1)
-                    i += inc;
+                    i += 2*(nRows-1);
                 else
                 {
-                    i += (down)?(dnInc):(upInc);
+                    i += (down)?(2*(nRows-row-1)):(2*row);
                    down = !down;
                 }
             }
