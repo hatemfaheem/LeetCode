fb6d8761ef81f05d012e0bb9e8e247015147a983
hatemfaheem <hatem.faheem@gmail.com>
2014-09-28 19:18:40 +0300

    AddBinary solved in O(n) without any extra space

--- AddBinary.cpp
+++ AddBinary.cpp
@@ -5,27 +5,19 @@
  *      Author: hatemfaheem
  */
 
-#include <iostream>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     string addBinary(string a, string b)
     {
-    	for (unsigned int i=0; i<a.size()-b.size(); i++)
-    		b = '0' + b;
-    	for (unsigned int i=0; i<b.size()-a.size(); i++)
-    		a = '0' + a;
-    	string res="";
-    	char rem='0';
-    	for (unsigned int i=a.size()-1; i>=0; i--)
-    	{
-    		int dig = (a[i]-'0')+(b[i]-'0')+(rem-'0');
-    		res = ((dig/2) + '0') + res;
-    		rem = ((dig%2)+'0');
-    	}
+        string res=""; char rem='0';
+        for (int i=a.size()-1, j=b.size()-1; i>=0 || j>=0; i--, j--)
+        {
+            char ai = ((i>=0)?(a[i]):('0')), bj = ((j>=0)?(b[j]):('0'));
+            int ax=ai-'0', bx=bj-'0', rx=rem-'0';
+            res = (char)((ax^bx^rx)+'0') + res;
+            rem = ((ax==1)?(bx|rx):(bx&rx))+'0';
+        }
+        return (rem=='1')?('1'+res):(res);
     }
 };
-
 
