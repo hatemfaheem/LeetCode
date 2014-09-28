c4dcb3ae3cb9c64503e82df905f017acf5526ef1
hatemfaheem <hatem.faheem@gmail.com>
2014-09-28 12:18:16 +0300

    2nd solution with O(m+n) space

--- SetMatrixZeroes.cpp
+++ SetMatrixZeroes.cpp
@@ -2,13 +2,24 @@ class Solution {
 public:
     void setZeroes(vector<vector<int> > &matrix)
     {
-        vector<vector<int> > tmpMatrix = matrix;
-        for (int i=0; i<tmpMatrix.size(); i++)
-            for (int j=0; j<tmpMatrix[i].size(); j++)
-                if (tmpMatrix[i][j] == 0)
+        int m = matrix.size(), n = matrix[0].size();
+        bool * setRow = new bool[m];
+        bool * setCol = new bool[n];
+        for (int i=0; i<m; i++) setRow[i] = false;
+        for (int i=0; i<n; i++) setCol[i] = false;
+        for (int i=0; i<m; i++)
+            for (int j=0; j<n; j++)
+                if (matrix[i][j] == 0)
                 {
-                    for (int k=0; k<matrix.size(); k++) matrix[k][j] = 0;
-                    for (int k=0; k<matrix[i].size(); k++) matrix[i][k] = 0;
+                    setRow[i] = true;
+                    setCol[j] = true;
                 }
+                
+        for (int i=0; i<m; i++)
+            for (int j=0; setRow[i] && j<n; j++)
+                matrix[i][j] = 0;
+        for (int j=0; j<n; j++)
+            for (int i=0; setCol[j] && i<m; i++)
+                matrix[i][j] = 0;
     }
 };
