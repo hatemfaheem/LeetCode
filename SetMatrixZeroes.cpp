bbc3d7ab05e55b255dfb19c5ef0e9419760c292f
hatemfaheem <hatem.faheem@gmail.com>
2014-09-28 12:47:35 +0300

    SetMatrixZeros solved with O(1) space O(n^2) time

--- SetMatrixZeroes.cpp
+++ SetMatrixZeroes.cpp
@@ -3,23 +3,26 @@ public:
     void setZeroes(vector<vector<int> > &matrix)
     {
         int m = matrix.size(), n = matrix[0].size();
-        bool * setRow = new bool[m];
-        bool * setCol = new bool[n];
-        for (int i=0; i<m; i++) setRow[i] = false;
-        for (int i=0; i<n; i++) setCol[i] = false;
+        bool firstRowZero=false, firstColZero=false;
         for (int i=0; i<m; i++)
             for (int j=0; j<n; j++)
                 if (matrix[i][j] == 0)
                 {
-                    setRow[i] = true;
-                    setCol[j] = true;
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                    firstRowZero |= (i==0);
+                    firstColZero |= (j==0);
                 }
                 
-        for (int i=0; i<m; i++)
-            for (int j=0; setRow[i] && j<n; j++)
+        for (int i=1; i<m; i++)
+            for (int j=1; (matrix[i][0]==0) && j<n; j++)
                 matrix[i][j] = 0;
-        for (int j=0; j<n; j++)
-            for (int i=0; setCol[j] && i<m; i++)
+        for (int j=1; j<n; j++)
+            for (int i=1; (matrix[0][j]==0) && i<m; i++)
                 matrix[i][j] = 0;
+        for (int i=0; firstColZero && i<m; i++)
+            matrix[i][0] = 0;
+        for (int j=0; firstRowZero && j<n; j++)
+            matrix[0][j] = 0;
     }
 };
