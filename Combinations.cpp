f7d7d0265f33e2891d3b87551d46ed1407d5e930
hatemfaheem <hatem.faheem@gmail.com>
2014-10-08 01:52:36 +0300

    Combinations solved with subsets style, N choose K, select subsets that has size K, prune when the current subset reaches size K

---
+++ Combinations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int> > combine(int n, int k)
+    {
+        vector<int> S;
+        for (int i=1; i<=n; i++)
+            S.push_back(i);
+        vector<vector<int> > combinations;
+        vector<int> comb;
+        generate(S, combinations, comb, 0, k);
+        return combinations;
+    }
+    
+    void generate(vector<int> &S, vector<vector<int> > & combinations, vector<int> currentComb, int index, int k)
+    {
+        if (currentComb.size() == k){
+            combinations.push_back(currentComb);
+            return;
+        }
+        if (index == S.size())
+            return;
+        generate(S, combinations, currentComb, index+1, k);
+        currentComb.push_back(S[index]);
+        generate(S, combinations, currentComb, index+1, k);
+    }
+};
