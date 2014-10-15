35c601f0c6e02e3b1288ff9ce1847fa4ce38b18f
hatemfaheem <hatem.faheem@gmail.com>
2014-10-15 15:46:54 +0300

    Single Number II solved with time 32*n which is O(n) and memory of 32 which is O(1)

--- SingleNumberII.cpp
+++ SingleNumberII.cpp
@@ -1,19 +1,38 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
     int singleNumber(int A[], int n)
     {
-        unordered_map<int,int> counts;
-        for (int i=0; i<n; i++)
-            if (counts.find(A[i]) != counts.end())
-                counts[A[i]] += 1;
-            else
-                counts[A[i]] = 1;
-        for (unordered_map<int,int>::iterator it=counts.begin(); it!=counts.end(); it++)
-            if (it->second != 3)
-                return it->first;
-        return -1;
+        int digits[32]; // digits sum
+        for (int d=0; d<32; d++)
+            digits[d]=0;
+        for (int d=0; d<32; d++)
+            for (int i=0; i<n; i++)
+                digits[d] += ((A[i] >> d) & 1);
+        int  result = 0;
+        for (int d=31; d>=0; d--){
+			result <<= 1;
+            result |= (digits[d]%3!=0);
+		}
+        return (result);
     }
 };
+
+// print binary representation for debugging
+void binary(int x)
+{
+	for (int i=31; i>=0; i--)
+		cout << ((x>>i)&1);
+	cout << "\n";
+}
+
+int main()
+{
+	int A[] = {-1/*2147483647*/,-2,-2,-2,-3,-3,-3}; // sample test case
+	Solution s;
+	cout << s.singleNumber2(A, 7) << "\n";
+	return 0;
+}
