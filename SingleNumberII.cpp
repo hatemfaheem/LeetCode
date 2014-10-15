7456dab0d5af8362cd710adf3bb3408b0ebca8d2
hatemfaheem <hatem.faheem@gmail.com>
2014-10-15 15:18:07 +0300

    Single Number II solved in O(n) time with O(n) memory using a hash table

--- SingleNumberII.cpp
+++ SingleNumberII.cpp
@@ -5,14 +5,15 @@ class Solution {
 public:
     int singleNumber(int A[], int n)
     {
-		int result = 0;
-        for (unsigned int i=0; i<n; i++)
-	        	result ^= A[i];
-        return result;
+        unordered_map<int,int> counts;
+        for (int i=0; i<n; i++)
+            if (counts.find(A[i]) != counts.end())
+                counts[A[i]] += 1;
+            else
+                counts[A[i]] = 1;
+        for (unordered_map<int,int>::iterator it=counts.begin(); it!=counts.end(); it++)
+            if (it->second != 3)
+                return it->first;
+        return -1;
     }
 };
-
-int main()
-{
-	return 0;
-}
