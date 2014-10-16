6a23ed84087d94a9a9fd6b2f241ad6b8873506f5
hatemfaheem <hatem.faheem@gmail.com>
2014-10-16 13:01:57 +0300

    Symmetric Tree solved using inorder traversal

--- SymmetricTree.cpp
+++ SymmetricTree.cpp
@@ -9,65 +9,33 @@
 #include <vector>
 using namespace std;
 
-/* Definition for binary tree */
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode *root) {
-    	vector<int> traversal, left, right;
-    	if (root == NULL)
-    		traversal.push_back(-1);
-    	else if (root->left == NULL && root->right == NULL)
-    		traversal.push_back(root->val);
-    	else
-    	{
-			left = inorderTraversal(root->left);
-			for (unsigned int i=0; i<left.size(); i++)
-				traversal.push_back(left[i]);
-			traversal.push_back(root->val);
-			right = inorderTraversal(root->right);
-			for (unsigned int i=0; i<right.size(); i++)
-				traversal.push_back(right[i]);
-    	}
-    	return traversal;
+    void inorderTraversal(TreeNode *root, vector<pair<int,int> > & traversal, int level)
+	{        
+    		if (root == NULL)
+		{
+    			traversal.push_back(pair<int,int>(-1, level));
+	    		return;
+	    	}
+    	
+		inorderTraversal(root->left, traversal, level+1);
+		traversal.push_back(pair<int,int>(root->val, level));
+		inorderTraversal(root->right, traversal, level+1);
+
+  	  	return;
     }
 
     bool isSymmetric(TreeNode *root)
     {
-    	vector<int> inorder = this->inorderTraversal(root);
-    	for (int i=0, j=inorder.size()-1; i<j; i++, j--)
-    		if (inorder[i]!=inorder[j])
-    			return false;
-    	return true;
+		vector<pair<int,int> > inorder;
+		this->inorderTraversal(root, inorder, 0);
+		for (int i=0, j=inorder.size()-1; i<j; i++, j--)
+			if ((inorder[i].first!=inorder[j].first) ||
+			    (inorder[i].second!=inorder[j].second))
+				return false;
+		return true;
     }
 };
-
-int main()
-{
-	TreeNode * n1 = new TreeNode(1);
-	TreeNode * n21 = new TreeNode(2);
-	TreeNode * n22 = new TreeNode(2);
-	TreeNode * n31 = new TreeNode(3);
-	TreeNode * n32 = new TreeNode(3);
-
-	n1->left = n21;
-	n1->right = n31;
-	n21->left = n32;
-	n31->left = n22;
-
-	Solution sol;
-	vector<int> v = sol.inorderTraversal(n1);
-	for (unsigned int i=0; i<v.size(); i++)
-		cout << v[i] << " ";
-	cout << "\n";
-
-	return 0;
-}
 
 
