66179b1809404941e3492c1409cb9a29adca534f
hatemfaheem <hatem.faheem@gmail.com>
2014-10-18 01:34:56 +0300

    Search 2D matrix solved in O(log(m)+log(n)) time

--- Searcha2DMatrix.cpp
+++ Searcha2DMatrix.cpp
@@ -5,60 +5,40 @@
  *      Author: hatemfaheem
  */
 
-#include <iostream>
-#include <vector>
-using namespace std;
 
 class Solution {
 public:
-	bool binarySearch(vector<int> & matrix, int start, int end, int target)
+    // search for the target in a row
+	bool targetBinarySearch(vector<int> & row, int start, int end, int target)
 	{
-		if (start < 0 || end >= matrix.size())
-			return false;
-		int mid = (start + end) / 2;
-		if (matrix[mid] == target)
-			return true;
-		else if (matrix[mid] > target)
-			return binarySearch(matrix, start, mid-1, target);
-		else
-			return binarySearch(matrix, mid+1, end, target);
+	    if (start > end)
+	        return false;
+	    int mid = (start+end)/2;
+	    if (row[mid]==target)
+	        return true;
+	    if (row[mid]>target)
+	        return targetBinarySearch(row, start, mid-1, target);
+	    return targetBinarySearch(row, mid+1, end, target);
+	}
+	
+	// search for the row that contains the target
+	int rowBinarySearch(vector<vector<int> > & matrix, int start, int end, int target, int n){
+	    if (start > end)
+	        return -1;
+	    int mid = (start+end)/2;
+	    if (matrix[mid][0] <= target && target <= matrix[mid][n-1])
+	        return mid;
+	    if (matrix[mid][0]>target)
+	        return rowBinarySearch(matrix, start, mid-1, target, n);
+	    return rowBinarySearch(matrix, mid+1, end, target, n);
 	}
 
-	bool search(vector<int> & matrix, int target)
-	{
-		for (unsigned int i=0; i<matrix.size(); i++)
-			if (target == matrix[i])
-				return true;
-		return false;
-	}
-
-    bool searchMatrix(vector<vector<int> > &matrix, int target) {
-    	unsigned int n = matrix.size();
-    	unsigned int m = matrix[0].size();
-    	if (n <= 0 || m <= 0 || matrix[0][0] > target || matrix[n-1][m-1] < target)
-    		return false;
-    	unsigned int row=0;
-    	for (; row<n; row++){
-    		if (matrix[row][0] <= target && target <= matrix[row][m-1])
-    			break;
-    	}
-    	if (row == n)
-    		return false;
-    	return search(matrix[row], target);
+    bool searchMatrix(vector<vector<int> > & matrix, int target) {
+    	unsigned int m = matrix.size();
+    	unsigned int n = matrix[0].size();
+    	if (m <= 0 || n <= 0) return false;
+    	int row = rowBinarySearch(matrix, 0, matrix.size()-1, target, n);
+    	if (row == -1) return false;
+    	return targetBinarySearch(matrix[row], 0, matrix[row].size()-1, target);
     }
 };
-
-/*
-int main()
-{
-	vector<vector<int> > matrix;
-	vector<int> v;
-	v.push_back(1);
-	//v.push_back(3);
-	//v.push_back(5);
-	matrix.push_back(v);
-	Solution sol;
-	cout << sol.searchMatrix(matrix, 1) << "\n";
-	return 0;
-}
-*/
