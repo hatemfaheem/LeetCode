c9c9a6a67491469cd2d2b9825571c21084acbb7d
hatemfaheem <hatem.faheem@gmail.com>
2014-10-18 00:49:38 +0300

    Max Depth in 1 line

--- MaximumDepthofBinaryTree.cpp
+++ MaximumDepthofBinaryTree.cpp
@@ -19,13 +19,7 @@ struct TreeNode {
 class Solution {
 public:
     int maxDepth(TreeNode *root) {
-    	if (root == NULL)
-    		return 0;
-    	int leftDepth = maxDepth(root->left);
-    	int rightDepth = maxDepth(root->right);
-    	if (leftDepth > rightDepth)
-    		return (leftDepth + 1);
-    	return (rightDepth + 1);
+        return (root == NULL)?(0):(max(maxDepth(root->left), maxDepth(root->right))+1);
     }
 };
 
